65eb87c48574537d74ca0a0790b3a8920133b775
Priyanshu Sinha <101971633+Hexadecimal-03@users.noreply.github.com>
2022-12-27 20:11:12 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,13 +1,3 @@
-"""Snake, classic arcade game.
-
-Exercises
-
-1. How do you make the snake faster or slower?
-2. How can you make the snake go around the edges?
-3. How would you move the food?
-4. Change the snake to respond to mouse clicks.
-"""
-
 from random import randrange
 from turtle import *
 
@@ -34,7 +24,17 @@ def move():
     head = snake[-1].copy()
     head.move(aim)
 
-    if not inside(head) or head in snake:
+    if not inside (head):
+        if head.x > 190:
+            head.x = -200
+        elif head.x < -200:
+            head.x = 190
+        elif head.y < -200:
+            head.y = 190
+        elif head.y > 190:
+            head.y = -200
+    
+    if head in snake:
         square(head.x, head.y, 9, 'red')
         update()
         return
@@ -62,9 +62,9 @@ setup(420, 420, 370, 0)
 hideturtle()
 tracer(False)
 listen()
-onkey(lambda: change(10, 0), 'Right')
-onkey(lambda: change(-10, 0), 'Left')
-onkey(lambda: change(0, 10), 'Up')
-onkey(lambda: change(0, -10), 'Down')
+onkey(lambda: change(10, 0), 'd')
+onkey(lambda: change(-10, 0), 'a')
+onkey(lambda: change(0, 10), 'w')
+onkey(lambda: change(0, -10), 's')
 move()
 done()
